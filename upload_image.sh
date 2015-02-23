c7a0fc4d456d976cc8296e7905b9bd94f840dbd3
John Chilton <jmchilton@gmail.com>
2015-02-22 23:50:33 -0500

    Tweak upload_image to allow suffix.

--- upload_image.sh
+++ upload_image.sh
@@ -7,7 +7,7 @@ else
 fi
 BOX=${BOX:-packer_virtualbox-iso_virtualbox.box}
 REMOTE_ROOT=${REMOTE_ROOT:-/srv/nginx/images.galaxyproject.org/root}
-
-scp $key_args $BOX sites@orval.galaxyproject.org:$REMOTE_ROOT/planemo/$time.box
-ssh $key_args sites@orval.galaxyproject.org bash -c "cd $REMOTE_ROOT/planemo; ln -f -s $time.box latest.box"
-
+SUFFIX=${SUFFIX:-""}
+NAME=$time$SUFFIX.box
+scp $key_args $BOX sites@orval.galaxyproject.org:$REMOTE_ROOT/planemo/$NAME
+ssh $key_args sites@orval.galaxyproject.org bash -c "cd $REMOTE_ROOT/planemo && ln -f -s $NAME latest.box"
