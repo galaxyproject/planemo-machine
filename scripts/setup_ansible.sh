77e44127f323ac1fc157bc25563d5f4fd480094b
Dannon Baker <dannon.baker@gmail.com>
2015-03-30 16:47:59 -0400

    retab to be consistent in setup_ansible.sh

--- scripts/setup_ansible.sh
+++ scripts/setup_ansible.sh
@@ -4,8 +4,8 @@
 dpkg -s ansible > /dev/null 2>&1
 if [ $? -ne 0 ];
 then
-	sudo apt-get update -y
-	sudo apt-get install -y software-properties-common
+    sudo apt-get update -y
+    sudo apt-get install -y software-properties-common
     sudo apt-get install -y python-dev
     sudo apt-get install -y python-pip
     sudo pip install ansible==1.9.0
